311f2d33ebb67d9c2e2fb1f72e340b391b1f3e05
CJ.Chen <imaxstone@outlook.com>
2022-12-25 14:42:32 +0800

    add

--- ifeastApp/Model/IfeastSetting.swift
+++ ifeastApp/Model/IfeastSetting.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 struct IfeastSetting: Codable{
-    let settingId: UUID?
+    let settingId: UUID? // 配置ID
+    let name: String? // 资源名称
+    let address: String? // IP地址
+    let port: Int16? // 端口
+    let path: String? //资源根路径
+    let account: String? // 账户
+    let password: String? // 密码
 }
